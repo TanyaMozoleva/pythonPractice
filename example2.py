f1c53c981abe78a339c16466337cc0c9af7237aa
TanyaMozoleva <tanya.mozoleva@gmail.com>
2020-04-23 23:49:40 +0300

    add a description

--- example2.py
+++ example2.py
@@ -1,16 +1,17 @@
+'''
+The following formula can be used to calculate the monthly repayments that need to be made to pay off a house loan
+'''
+
 loan_amount = 650000
 annual_interest_rate = 5
 loan_length_years = 20
 months_per_year = 12
 
-
 month_interest_rate = annual_interest_rate / months_per_year
-amount_of_months = loan_length_years * months_per_year
-percent_of_rate = month_interest_rate / 100
-numerator = percent_of_rate * ( 1 + percent_of_rate) ** amount_of_months
-denominator = ((1 + percent_of_rate) ** amount_of_months) - 1 
+months = loan_length_years * months_per_year
+percentage_of_rate = month_interest_rate / 100
+numerator = percentage_of_rate * ( 1 + percentage_of_rate) ** months
+denominator = ((1 + percentage_of_rate) ** months) - 1 
 monthly_payment = loan_amount * (numerator / denominator)
 
-
 print('You will need to pay $', round(monthly_payment), ' monthly', sep = '')
-
