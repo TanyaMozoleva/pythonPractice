b9b6e8e22641681a966acbceb8af033e28657d12
TanyaMozoleva <tanya.mozoleva@gmail.com>
2020-04-27 21:15:46 +0300

    Programm that calculates the area of a circle

---
+++ example3.py
@@ -0,0 +1,9 @@
+'''
+Complete the following program that calculates the area of a circle.
+'''
+
+import math
+radius = 7 
+area = math.pi * (radius ** 2)
+
+print("The area of the circle is ", round(area), sep = "")
