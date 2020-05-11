e724d09af959160c9b799eebbfe94b103d3c0eab
TanyaMozoleva <tanya.mozoleva@gmail.com>
2020-05-11 16:07:59 +0300

    add changes

---
+++ Weeks/week2/w2t5.py
@@ -0,0 +1,13 @@
+'''
+Write a program that prompts the user to enter a word. 
+In then prints a new word where the first and last characters of the word
+entered by the user are swapped.
+'''
+
+word = input('Enter a word: ')
+first_character = word[:1]
+last_character = word[-1:]
+middle_slice = word[1:-1]
+new_word = last_character + middle_slice + first_character
+
+print('The new word is ', new_word, sep = '')
