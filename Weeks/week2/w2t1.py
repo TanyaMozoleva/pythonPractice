0f958621adebb4f3799aa40a892a6796d80894eb
TanyaMozoleva <tanya.mozoleva@gmail.com>
2020-05-11 12:01:27 +0300

    fix

--- Weeks/week2/w2t1.py
+++ Weeks/week2/w2t1.py
@@ -2,14 +2,13 @@
 Write a program, that prompts the user to enter ther name and then displays their name, in uppercase,
 as part of a banner.
 '''
+
 sign = '#'
 first_name = input('Enter your first name: ')
 family_name = input('Enter your family name: ')
 full_name = first_name.upper() + ' ' + family_name.upper()
 line_of_signs = sign * len(full_name)
 
-#print(first_name)
-#print(family_name)
 print(line_of_signs)
 print(full_name)
 print(line_of_signs)
