b16da1b2315dfc01ee9a54395e32569f5d918ad7
TanyaMozoleva <tanya.mozoleva@gmail.com>
2020-05-28 21:36:18 +0300

    changes

--- Weeks/week3/w3t7.py
+++ Weeks/week3/w3t7.py
@@ -1,4 +1,7 @@
 '''
+obtain a number from the user( in the  function call use the request variable defined in the main)
+calculate the cost of the items(including the handling)
+print the cost details
 '''
 def get_number(prompt):
     return int(input(prompt))
@@ -8,12 +11,13 @@ def get_cost(number_of_items, cost_per_unit, handling_cost):
     return total_cost
 
 def display_details(items, cost_each, handling_cost, final_price):
-
-def main():
-    prompt = input('Enter number (5-20): ')
-    items = get_number(prompt)
     print('Items: ', items, 'Cost per item: $', cost_each, sep='')
     print('Handling cost: $', handling_cost, sep='')
     print('Total: $', final_price, sep='')
+
+def main():
+    request = ('Enter number (5-20): ')
+    items = get_number(request)
+    display_details()
 
 main()
